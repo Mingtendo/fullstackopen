c7541045fc755142ec7e3f457a0da181f36afadb
Mingtendo <mingtendo@gmail.com>
2023-06-29 17:33:57 -0400

    Final commit before Part 1c - page re-rendering.

--- part_1/part1/src/App.js
+++ part_1/part1/src/App.js
@@ -1,11 +1,14 @@
 
 // Component Hello. Components are defined as JS functions.
-const Hello = (props) =>
+const Hello = ({name, age}) =>
 {
-	console.log(props)
+	const bornYear = () => new Date().getFullYear() - age;
+
+	console.log({name}, {age});
 	return (
 		<div>
-			<p>Hello {props.name}, you are {props.age} years old</p>
+			<p>Hello {name}, you are {age} years old</p>
+			<p>So you were probably born in {bornYear()}</p>
 		</div>
 	)
 }
@@ -22,16 +25,14 @@ const Footer = () =>
 const App = () =>
 {
 
-	const friends = 
-	[
-		{name: 'Peter', age: 4},
-		{name: 'Maya', age: 10}
-	]
+	const name = 'Peter';
+	const age = 10;
 
 	return (
 		<div>
-			<p>{friends[0].name} {friends[0].age}</p>
-			<p>{friends[1].name} {friends[1].age}</p>
+			<h1>Greetings</h1>
+			<Hello name="Maya" age={26+10} />
+			<Hello name={name} age={age} />
 		</div>
 	)
 }
