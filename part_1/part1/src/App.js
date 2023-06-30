8dcaf3055c8f94c8704fba951bb1dc2d27bf0912
Mingtendo <mingtendo@gmail.com>
2023-06-29 22:18:18 -0400

    Commented out unused code from earlier parts to avoid compiling warnings. All lesson code is up-to-date to the end of Part 1c.

--- part_1/part1/src/App.js
+++ part_1/part1/src/App.js
@@ -1,38 +1,61 @@
-
+import {useState} from 'react';
 // Component Hello. Components are defined as JS functions.
-const Hello = ({name, age}) =>
-{
-	const bornYear = () => new Date().getFullYear() - age;
+// const Hello = ({name, age}) =>
+// {
+// 	const bornYear = () => new Date().getFullYear() - age
 
-	console.log({name}, {age});
+// 	console.log({name}, {age})
+// 	return (
+// 		<div>
+// 			<p>Hello {name}, you are {age} years old</p>
+// 			<p>So you were probably born in {bornYear()}</p>
+// 		</div>
+// 	)
+// }
+
+// const Footer = () =>
+// {
+// 	return (
+// 		<div>
+// 			greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+// 		</div>
+// 	)
+// }
+
+const Display = ({counter}) => <div>{counter}</div>
+	
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+const App = (props) =>
+{
+	const [counter, setCounter] = useState(0)
+	console.log('Rendering with counter value', counter)
+
+	const increaseByOne = () => 
+	{
+		console.log('Increasing, value before', counter) 
+		setCounter(counter+1)
+	}
+
+	const decreaseByOne = () => 
+	{
+		console.log('Decreasing, value before', counter)
+		setCounter(counter-1)
+	}
+
+	const setToZero = () => 
+	{
+		console.log('Resetting to zero, value before', counter)
+		setCounter(0)
+	}
+
 	return (
 		<div>
-			<p>Hello {name}, you are {age} years old</p>
-			<p>So you were probably born in {bornYear()}</p>
-		</div>
-	)
-}
-
-const Footer = () =>
-{
-	return (
-		<div>
-			greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
-		</div>
-	)
-}
-
-const App = () =>
-{
-
-	const name = 'Peter';
-	const age = 10;
-
-	return (
-		<div>
-			<h1>Greetings</h1>
-			<Hello name="Maya" age={26+10} />
-			<Hello name={name} age={age} />
+			<Display counter={counter} />
+			<Button handleClick={increaseByOne} text='Plus' />
+			<Button handleClick={decreaseByOne} text='Minus' />
+			<Button handleClick={setToZero} text='Reset' />
 		</div>
 	)
 }
