78049516ea01fd0a1320367b86af377eab93417e
Mingtendo <mingtendo@gmail.com>
2023-08-02 21:27:08 -0400

    Started Part 2a, up to the Map section.

--- part_2/part2/src/App.js
+++ part_2/part2/src/App.js
@@ -1,10 +1,20 @@
-const App = () =>
+const App = (props) =>
 {
-	
+	const {notes} = props
 
+	console.log('props value is', props)
+
+	// Items in a list must have a unique ID, or key attribute.
 	return (
 		<div>
-			Hello There!
+			<h1>Notes</h1>
+			<ul>
+				{notes.map((note) => 
+					<li key={note.id}>
+						{note.content}
+					</li>
+				)}
+			</ul>
 		</div>
 	)
 }
--- part_2/part2/src/index.js
+++ part_2/part2/src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+const notes =
+[
+	{
+		id: 1,
+		content: 'HTML is easy',
+		important: true
+	},
+	{
+		id: 2,
+		content: 'Browser can execute only JavaScript',
+		important: false
+	},
+	{
+		id: 3,
+		content: 'GET and POST are the most important methods of HTTP protocol',
+		important: true
+	}
+]
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<App notes={notes} />
+)
 
+const result = notes.map(note => note.id)
+console.log("output of result", result)
