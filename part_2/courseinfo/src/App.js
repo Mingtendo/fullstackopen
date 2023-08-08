596adabddf36ba256daef3c6bf7edc1bb594d855
Mingtendo <mingtendo@gmail.com>
2023-08-08 19:52:10 -0400

    Got my code to display parts of a course in an unordered list, using the map() method. Should work with an arbitrary number of parts, but the output needs to be changed to match the given output.

--- part_2/courseinfo/src/App.js
+++ part_2/courseinfo/src/App.js
@@ -1,50 +1,66 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
+const Header = ({ header }) => 
+{
+	console.log('header data', header)
+	return (
+		<h1>{header}</h1>
+	)
+}
 const Part = ({ part }) => 
-	<p>
+	<li>
     	{part.name} {part.exercises}
-  	</p>
+  	</li>
 
-const Content = ({ parts }) => 
-  	<>
-    	<Part
-      		part={parts[0]} 
-    	/>
-    	<Part
-      		part={parts[1]} 
-    	/>
-    	<Part
-      		part={parts[2]} 
-    	/>      
-  	</>
+const Content = ({content}) =>
+{
+	return (
+		<ul>
+			{content.map((note) =>
+				<Part key={note.id} part={note}/>
+			)}
+		</ul>
+	)
+}
 
-const App = () => {
-  	const course = 'Half Stack application development'
-  	const parts = 
-	[
-    	{
-      		name: 'Fundamentals of React',
-      		exercises: 10
-    	},
-    	{
-      		name: 'Using props to pass data',
-      		exercises: 7
-    	},
-    	{
-      		name: 'State of a component',
-      		exercises: 14
-    	}
-  	]
+const Course = ({course}) => 
+{
+	console.log('data in parts as given to Course component', course)
+	return (
+		<div>
+			<Header header={course.name}/>
+			<Content content={course.parts} />
+		</div>
+		
+	)
+}
 
-  	return (
-    	<div>
-      		<Header course={course} />
-      		<Content parts={parts} />
-      		<Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
-    	</div>
-  	)
+const App = () => 
+{
+  	const course = 
+	{
+		id: 1,
+		name: 'Half Stack application development',
+		parts:
+		[
+			{
+				  name: 'Fundamentals of React',
+				  exercises: 10,
+				  id: 1
+			},
+			{
+				  name: 'Using props to pass data',
+				  exercises: 7,
+				  id: 2
+			},
+			{
+				  name: 'State of a component',
+				  exercises: 14,
+				  id: 3
+			}
+		]
+	}
+	
+
+  	return <Course course={course} />
 }
 
 export default App
