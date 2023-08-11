19178b5569074db2066d1aa9cf03d95b1ea7754c
Mingtendo <mingtendo@gmail.com>
2023-08-11 15:15:22 -0400

    Added functionality to support multiple courses and their parts. Stable to exercise 2.4.

--- part_2/courseinfo/src/App.js
+++ part_2/courseinfo/src/App.js
@@ -57,37 +57,62 @@ const Course = ({course}) =>
 
 const App = () => 
 {
-  	const course = 
-	{
-		id: 1,
-		name: 'Half Stack application development',
-		parts:
-		[
-			{
-				name: 'Fundamentals of React',
-				exercises: 10,
-				id: 1
-			},
-			{
-				name: 'Using props to pass data',
-				exercises: 7,
-				id: 2
-			},
-			{
-				name: 'State of a component',
-				exercises: 14,
-				id: 3
-			},
-			{
-				name: 'Array fundamentals',
-				exercises: 69,
-				id: 4
-			}
-		]
-	}
+  	const courses = 
+	[
+		{
+			id: 1,
+			name: 'Half Stack application development',
+			parts:
+			[
+				{
+					name: 'Fundamentals of React',
+					exercises: 10,
+					id: 1
+				},
+				{
+					name: 'Using props to pass data',
+					exercises: 7,
+					id: 2
+				},
+				{
+					name: 'State of a component',
+					exercises: 14,
+					id: 3
+				},
+				{
+					name: 'Redux',
+					exercises: 11,
+					id: 4
+				}
+			]
+		},
+		{
+			name: 'Node.js',
+			id: 2,
+			parts:
+			[
+				{
+					name: 'Routing',
+					exercises: 3,
+					id: 1
+				},
+				{
+					name: 'Middlewares',
+					exercises: 7,
+					id: 2
+				}
+			]
+		}
+	]
 	
 
-  	return <Course course={course} />
+  	return (
+		<div>
+			{courses.map((course) =>
+				<Course key={course.id} course={course} />
+			)}
+		</div>
+	)
 }
 
 export default App
