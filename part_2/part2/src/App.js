f730c3bb51d999ed7b96b083ddf532a13720a431
Mingtendo <mingtendo@gmail.com>
2023-08-26 18:07:44 -0400

    Added ability to view only important notes.

--- part_2/part2/src/App.js
+++ part_2/part2/src/App.js
@@ -5,6 +5,7 @@ const App = (props) =>
 {
 	const [notes, setNotes] = useState(props.notes)
 	const [newNote, setNewNote] = useState('a new note...')
+	const [showAll, setShowAll] = useState(false)
 
 	console.log('content of notes is: ', notes)
 
@@ -31,12 +32,21 @@ const App = (props) =>
 		setNewNote(event.target.value)
 	}
 
+	const notesToShow = showAll
+		? notes
+		: notes.filter(note => note.important === true)
+
 	// Items in a list must have a unique ID, or key attribute.
 	return (
 		<div>
 			<h1>Notes</h1>
+			<div>
+				<button onClick={() => setShowAll(!showAll)}>
+					show {showAll ? 'important' : 'all'}
+				</button>
+			</div>
 			<ul>
-				{notes.map((note) => 
+				{notesToShow.map((note) => 
 					<Note key={note.id} note={note} />
 				)}
 			</ul>
