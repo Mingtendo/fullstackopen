da95a30f05e050028e7cc6e19f00fe87fa45b07e
Mingtendo <mingtendo@gmail.com>
2023-09-08 15:47:20 -0400

    Converted existing Notes app (the lesson code) from a CRA project to a Vite project. It now resides in /part_2/part2Vite. The other folder will be kept for archive purposes.

---
+++ part_2/part2Vite/src/App.jsx
@@ -0,0 +1,64 @@
+import {useState} from 'react'
+import Note from "./components/Note"
+
+const App = (props) =>
+{
+	const [notes, setNotes] = useState(props.notes)
+	const [newNote, setNewNote] = useState('a new note...')
+	const [showAll, setShowAll] = useState(false)
+
+	console.log('content of notes is: ', notes)
+
+	const addNote = (event) => 
+	{
+		event.preventDefault()
+		console.log('button clicked', event.target)
+
+		const noteObject = 
+		{
+			content: newNote,
+			important: Math.random() < 0.5,
+			id: notes.length+1
+		}
+
+		setNotes(notes.concat(noteObject))
+		setNewNote('')
+	}
+
+	// Changes the value of newNote, which is the value in the input bar everytime something is removed or added
+	const handleNoteChange = (event) =>
+	{
+		console.log(event.target.value)
+		setNewNote(event.target.value)
+	}
+
+	const notesToShow = showAll
+		? notes
+		: notes.filter(note => note.important === true)
+
+	// Items in a list must have a unique ID, or key attribute.
+	return (
+		<div>
+			<h1>Notes</h1>
+			<div>
+				<button onClick={() => setShowAll(!showAll)}>
+					show {showAll ? 'important' : 'all'}
+				</button>
+			</div>
+			<ul>
+				{notesToShow.map((note) => 
+					<Note key={note.id} note={note} />
+				)}
+			</ul>
+			<form onSubmit={addNote}>
+				<input 
+					value={newNote}
+					onChange={handleNoteChange}
+				/>
+				<button type='submit'>save</button>
+			</form>
+		</div>
+	)
+}
+
+export default App
---
+++ part_2/part2Vite/src/components/Note.jsx
@@ -0,0 +1,8 @@
+const Note = ({note}) =>
+{
+	return (
+		<li>{note.content}</li>
+	)
+}
+
+export default Note
