23ac94988fca209f874cbed5dc7bf4a5f8dea797
Mingtendo <mingtendo@gmail.com>
2023-09-15 15:30:20 -0400

    C:/Program Files/Git/part_2/part2Vite is now up to date with all the lesson code.

--- part_2/part2Vite/src/App.jsx
+++ part_2/part2Vite/src/App.jsx
@@ -1,13 +1,28 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import Note from "./components/Note"
 
-const App = (props) =>
+const App = () =>
 {
-	const [notes, setNotes] = useState(props.notes)
+	const [notes, setNotes] = useState([])
 	const [newNote, setNewNote] = useState('a new note...')
-	const [showAll, setShowAll] = useState(false)
+	const [showAll, setShowAll] = useState(true)
 
-	console.log('content of notes is: ', notes)
+	const hook = () =>
+	{
+		console.log('effect')
+		axios
+			.get('http://localhost:3001/notes')
+			.then(response => 
+			{
+				console.log('promise fulfilled')
+				setNotes(response.data)
+			})
+	}
+
+	useEffect(hook, [])
+
+	console.log('render', notes.length, 'notes')
 
 	const addNote = (event) => 
 	{
--- part_2/part2Vite/src/main.jsx
+++ part_2/part2Vite/src/main.jsx
@@ -1,46 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import axios from 'axios'
 import App from './App'
-
-const notes =
-[
-	{
-		id: 1,
-		content: 'HTML is easy',
-		important: true
-	},
-	{
-		id: 2,
-		content: 'Browser can execute only JavaScript',
-		important: false
-	},
-	{
-		id: 3,
-		content: 'GET and POST are the most important methods of HTTP protocol',
-		important: true
-	}
-]
-
-// Shows how promises work.
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
-
-axios
-	.get('http://localhost:3001/notes')
-	.then(response =>
-	{
-		const notes = response.data
-		console.log('uhhh notes promise', notes)
-	})
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<App notes={notes} />
+	<App />
 )
-
-const result = notes.map(note => note.id)
-console.log("output of result", result)
