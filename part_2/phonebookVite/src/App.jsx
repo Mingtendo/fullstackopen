dff8647fd73eb69cde3ab6f5c04a70312360682e
Mingtendo <mingtendo@gmail.com>
2023-09-15 17:42:51 -0400

    Changed App.jsx to fetch data from the JSON-server and set it to the persons variable as the initial state. Removed initial data for persons variable as all data is in the database now. Stable for Exercise 2.11, but the app doesn't work yet.

--- part_2/phonebookVite/src/App.jsx
+++ part_2/phonebookVite/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import axios from 'axios'
 
 const Field = (props) =>
 {
@@ -61,25 +62,25 @@ const PersonsList = (props) =>
 
 const App = () =>
 {
-	const [persons, setPersons] = useState([
-		{ 
-			name: 'Arto Hellas', number: '040-1234567',id: 1
-		},
-		{
-			name: 'Ada Lovelace', number: '39-44-5323523', id: 2
-		},
-		{
-			name: 'Dan Abramov', number: '12-43-234345', id: 3
-		},
-		{
-			name: 'Mary Poppendieck', number: '39-23-6423122', id: 4
-		}
-	])
-
+	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newNumb, setNewNumb] = useState('')
 	const [idCount, setIDCount] = useState(5)
 	const [search, setSearch] = useState('')
+
+	const hook = () =>
+	{
+		console.log('effect')
+		axios
+			.get('http://localhost:3001/persons')
+			.then(response =>
+			{
+				console.log('promise fulfilled')
+				setPersons(response.data)
+			})
+	}
+
+	useEffect(hook, [])
 
 	const addName = (event) =>
 	{
