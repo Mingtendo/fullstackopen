86651b503a8a8240a354b14edbdce227f9acffb3
Mingtendo <mingtendo@gmail.com>
2023-09-16 19:14:14 -0400

    Code up-to-date with 'Changing the Importance of Notes'. Also added some comments to clarify what different parts do.

--- part_2/part2Vite/src/App.jsx
+++ part_2/part2Vite/src/App.jsx
@@ -5,9 +5,10 @@ import Note from "./components/Note"
 const App = () =>
 {
 	const [notes, setNotes] = useState([])
-	const [newNote, setNewNote] = useState('a new note...')
+	const [newNote, setNewNote] = useState('')
 	const [showAll, setShowAll] = useState(true)
 
+	// Fetches data from server upon startup, and every time something is re-rendered thereafter (component update, etc)
 	const hook = () =>
 	{
 		console.log('effect')
@@ -21,23 +22,34 @@ const App = () =>
 	}
 
 	useEffect(hook, [])
-
 	console.log('render', notes.length, 'notes')
 
+	const toggleImportanceOf = (id) =>
+	{
+		console.log(`importance of ${id} needs to be toggled`)
+	}
+
+	// Adds a note to the server, then fetches the list of notes from the server
 	const addNote = (event) => 
 	{
 		event.preventDefault()
 		console.log('button clicked', event.target)
 
+		// ID removed, as it's better for the server to handle this.
 		const noteObject = 
 		{
 			content: newNote,
-			important: Math.random() < 0.5,
-			id: notes.length+1
+			important: Math.random() < 0.5
 		}
 
-		setNotes(notes.concat(noteObject))
-		setNewNote('')
+		axios
+			.post('http://localhost:3001/notes', noteObject)
+			.then(response =>
+			{
+				console.log(response)
+				setNotes(notes.concat(response.data))
+				setNewNote('')
+			})
 	}
 
 	// Changes the value of newNote, which is the value in the input bar everytime something is removed or added
@@ -62,7 +74,11 @@ const App = () =>
 			</div>
 			<ul>
 				{notesToShow.map((note) => 
-					<Note key={note.id} note={note} />
+					<Note 
+						key={note.id} 
+						note={note} 
+						toggleImportance = {() => toggleImportanceOf(note.id)}
+					/>
 				)}
 			</ul>
 			<form onSubmit={addNote}>
